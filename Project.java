303f554b6ac1f22307612092eabeedea179987c7
UEM <dipanmukherjee8482@gmail.com>
2025-03-20 16:11:06 +0530

    mycommit

---
+++ Project.java
@@ -0,0 +1,17 @@
+public class {
+	
+    public static void main(String[] args){
+        int a=12;
+        int b=23;
+
+
+      	a=a*b;
+	b=a/b;
+	a=a/b;
+
+	System.out.print("value of a: "+a);
+	System.out.print("value of b: "+b);
+        
+    }
+
+}
